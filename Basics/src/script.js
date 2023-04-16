21820cf30569dc7a6fd1d1a2ec581e40a005a018
muridot0 <muri0@outlook.com>
2023-04-16 22:07:23 +0100

    completed animations lesson

--- Basics/src/script.js
+++ Basics/src/script.js
@@ -1,5 +1,6 @@
 import './style.css'
 import * as THREE from 'three'
+import gsap from 'gsap'
 
 console.log(THREE)
 
@@ -57,5 +58,18 @@ const renderer = new THREE.WebGLRenderer({
 
 renderer.setSize(sizes.width, sizes.height)
 
-renderer.render(scene, camera)
+const clock = new THREE.Clock()
+
+gsap.to(group.position, { duration: 1, delay: 1, x: 2 } )
+gsap.to(group.position, { duration: 1, delay: 2, x: 0 } )
+
+//Animations
+const tick = () => {
+    
+
+    renderer.render(scene, camera)
+    window.requestAnimationFrame(tick)
+}
+
+tick()
 
