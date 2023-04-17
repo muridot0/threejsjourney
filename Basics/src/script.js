e3b85096c071891155464d3e9f262f827c97eab7
muridot0 <muri0@outlook.com>
2023-04-17 21:32:34 +0100

    completed the cameras lesson

--- Basics/src/script.js
+++ Basics/src/script.js
@@ -1,75 +1,84 @@
-import './style.css'
-import * as THREE from 'three'
-import gsap from 'gsap'
+import "./style.css";
+import * as THREE from "three";
+import gsap from "gsap";
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
-console.log(THREE)
+//Canvas
+const canvas = document.querySelector(".webgl");
 
-const scene = new THREE.Scene()
-
-//Cube Group
-const group = new THREE.Group()
-group.position.set(0, 1, 0)
-group.scale.set(1, 2, 1)
-group.rotation.set(0, Math.PI, 0)
-scene.add(group)
-
-const cube1 = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial({ color: 0xff0000})
-)
-
-group.add(cube1)
-
-const cube2 = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial({ color: 0x00ff00})
-)
-
-cube2.position.set(-2, 0, 0)
-
-group.add(cube2)
-
-const cube3 = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial({ color: 0x0000ff})
-)
-
-cube3.position.set(2, 0, 0)
-
-group.add(cube3)
-
-const axesHelper = new THREE.AxesHelper()
-
-scene.add(axesHelper)
-
-const sizes = {
-    width: 800,
-    height: 600
+// Cursor
+const cursor = {
+    x: 0,
+    y: 0
 }
-
-const camera = new THREE.PerspectiveCamera(75, sizes.width/sizes.height)
-scene.add(camera)
-camera.position.set(0, 0, 3)
-
-const canvas = document.querySelector('.webgl')
-const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
+window.addEventListener('mousemove', (event) => {
+    cursor.x = event.clientX / sizes.width - 0.5
+    cursor.y = - (event.clientY / sizes.height - 0.5)
 })
 
-renderer.setSize(sizes.width, sizes.height)
+const scene = new THREE.Scene();
 
-const clock = new THREE.Clock()
+//Cube Group
+const group = new THREE.Group();
+group.position.set(0, 0, 0);
+group.scale.set(1, 1, 1);
+scene.add(group);
 
-gsap.to(group.position, { duration: 1, delay: 1, x: 2 } )
-gsap.to(group.position, { duration: 1, delay: 2, x: 0 } )
+const cube1 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1, 5, 5, 5),
+  new THREE.MeshBasicMaterial({ color: 0xff0000 })
+);
+
+group.add(cube1);
+
+const cube2 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1, 5, 5, 5),
+  new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+);
+
+cube2.position.set(-2, 0, 0);
+
+group.add(cube2);
+
+const cube3 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1, 5, 5, 5),
+  new THREE.MeshBasicMaterial({ color: 0x0000ff })
+);
+
+cube3.position.set(2, 0, 0);
+
+group.add(cube3);
+
+const axesHelper = new THREE.AxesHelper();
+
+scene.add(axesHelper);
+
+const sizes = {
+  width: 800,
+  height: 600,
+};
+
+const camera = new THREE.PerspectiveCamera(75, sizes.width/sizes.height, 0.1, 100)
+scene.add(camera);
+camera.position.set(0, 0, 3);
+camera.lookAt(group.position);
+
+//Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+
+renderer.setSize(sizes.width, sizes.height);
 
 //Animations
 const tick = () => {
-    
+  controls.update()
 
-    renderer.render(scene, camera)
-    window.requestAnimationFrame(tick)
-}
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(tick);
+};
 
-tick()
-
+tick();
